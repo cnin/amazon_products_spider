7abeead36de8133388a755596d4f1c6c6a3c18e0
brvcalf <brvcalf@gmail.com>
2017-07-30 22:58:16 +0800

    intial version for task4

--- page_parse_example.py
+++ page_parse_example.py
@@ -1,44 +1,43 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 import requests
 import lxml.html
+
 
 class TargetSelect:
     def __init__(self, name=None, select_term=None):
         self.name = name
         self.select_term = select_term
 
-class TargetInfo:
-    def __init__(self, name=None, info=None):
-        self.name = name
-        self.info = info
-
 title = TargetSelect('product_title', 'h1#title > span#productTitle')
 td = TargetSelect('area', 'tr#places_area__row > td.w2p_fw')
-# asin = TargetSelect('ASIN', '')
-# price = TargetSelect('price', 'tr#priceblock_ourprice_row > span#priceblock_ourprice')
+size = TargetSelect('memory', 'div#variation_size_name > div.a-row > span.selection')
+color = TargetSelect('color', 'div#variation_color_name > div.a-row > span.selection')
+soldby = TargetSelect('soldby', 'div#shipsFromSoldBy_feature_div > div#merchant-info')
 price = TargetSelect('price', 'tr#priceblock_ourprice_row > td.a-span12 > span#priceblock_ourprice')
-
+timestamp = TargetSelect('timestamp', '{:%Y%m%d%H%M%S}')
 
 def scrape(html):
-    targets = [title, price]
-    # targets = [td, price]
-    results = [TargetInfo(ele.name, None) for ele in targets ]
+    targets = [title, size, color, soldby, price ]
+    results = {}
+    for ele in targets:
+        results[ele.name] = None
     tree = lxml.html.fromstring(html)
-    # treestr = lxml.html.tostring(tree, pretty_print=True)
-    # print(treestr)
-    for i in range(len(targets) ):
-        ele = tree.cssselect(targets[i].select_term )
-        if ele:
-            results[i].info = ele[0].text_content().strip()
+    for ele in targets:
+        select_res = tree.cssselect(ele.select_term )
+        if select_res:
+            results[ele.name] = " ".join(select_res[0].text_content().split() )
         else:
-            results[i].info = '*** failed ***'
-        print(results[i].name + ': ' + results[i].info)
+            results[ele.name] = '*** failed ***'
+        print(ele.name + ': ' + results[ele.name])
+    results[timestamp.name] = timestamp.select_term.format(datetime.now() )
     return results
 
 if __name__ == '__main__':
     seed_url = 'https://www.amazon.com/dp/B01J8PBEUM?th=1'
-    # seed_url = 'http://example.webscraping.com/places/default/view/United-Kingdom-239'
     response = requests.get(seed_url)
     html = response.text 
-    scrape(html)
+    # html = '<a></a>'
+    results = scrape(html)
+    print(timestamp.name + ': ' + results[timestamp.name] )
